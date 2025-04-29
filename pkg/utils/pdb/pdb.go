91c3092fdfc9940aabf188c8c8dd8e86f4ad4df9
Rodrigo Fior Kuntzer <rodrigo@miro.com>
2025-04-29 08:17:12 +0200

    Revert "fix: handle case where disruptionsAllowed is zero in fully blocking PDB logic"
    This reverts commit b258cf26d151fb076f7ae214c4625d6675902157.

--- pkg/utils/pdb/pdb.go
+++ pkg/utils/pdb/pdb.go
@@ -120,7 +120,7 @@ func (l Limits) isEvictable(pod *v1.Pod, evictionBlocker evictionBlocker) (clien
 						return pdb.key, false
 					}
 				case fullyBlockingPDBs:
-					if pdb.isFullyBlocking || pdb.disruptionsAllowed == 0 {
+					if pdb.isFullyBlocking {
 						return pdb.key, false
 					}
 				}
